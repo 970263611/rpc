cd503e21c73682ced7d17259178db4fe89df9f19
dingwq <dingwq@i2finance.net>
2020-01-14 17:29:12 +0800

    优化netty粘包拆包方法

--- boke-rpc/src/main/java/com/dahuaboke/rpc/common/RpcDecoder.java
+++ boke-rpc/src/main/java/com/dahuaboke/rpc/common/RpcDecoder.java
@@ -7,39 +7,35 @@ import io.netty.handler.codec.ByteToMessageDecoder;
 
 import java.util.List;
 
-/**
- * @author y15079
- * @create 2018-03-31 23:50
- * @desc
- * RPC 解码器
- **/
-public class RpcDecoder extends ByteToMessageDecoder{
-	private Class<?> genericClass;
+public class RpcDecoder extends ByteToMessageDecoder {
+    private Class<?> cls;
 
-	//构造函数传入反序列化的class
-	public RpcDecoder(Class<?> genericClass) {
-		this.genericClass = genericClass;
-	}
+    //构造函数传入反序列化的class
+    public RpcDecoder(Class<?> cls) {
+        this.cls = cls;
+    }
 
-	@Override
-	protected void decode(ChannelHandlerContext channelHandlerContext, ByteBuf in, List<Object> out) throws Exception {
-		if (in.readableBytes() < 4){
-			return;
-		}
-		in.markReaderIndex();
-		int dataLength = in.readInt();
-		if (dataLength < 0){
-			channelHandlerContext.close();
-		}
-		if (in.readableBytes() < dataLength){
-			in.resetReaderIndex();
-			return;
-		}
-		//将ByteBuf转换为byte[]
-		byte[] data = new byte[dataLength];
-		in.readBytes(data);
-		//将data转成object
-		Object obj = SerializationUtil.deserialize(data, genericClass);
-		out.add(obj);
-	}
+    @Override
+    protected void decode(ChannelHandlerContext channelHandlerContext, ByteBuf in, List<Object> out) {
+
+        if (in.readableBytes() < 4) {
+            return;
+        }
+        in.markReaderIndex();
+        int dataLength = in.readInt();
+        if (dataLength < 0) {
+            channelHandlerContext.close();
+        }
+        if (in.readableBytes() < dataLength) {
+            in.resetReaderIndex();
+            return;
+        }
+
+        byte[] data = new byte[dataLength];
+        in.readBytes(data);
+
+        Object obj = SerializationUtil.deserialize(data, cls);
+        out.add(obj);
+    }
+
 }
--- boke-rpc/src/main/java/com/dahuaboke/rpc/common/RpcEncoder.java
+++ boke-rpc/src/main/java/com/dahuaboke/rpc/common/RpcEncoder.java
@@ -5,28 +5,24 @@ import io.netty.buffer.ByteBuf;
 import io.netty.channel.ChannelHandlerContext;
 import io.netty.handler.codec.MessageToByteEncoder;
 
-/**
- * @author y15079
- * @create 2018-04-01 1:01
- * @desc
- * RPC 编码器
- **/
 public class RpcEncoder extends MessageToByteEncoder{
 
-	private Class<?> genericClass;
+	private Class<?> cls;
 
 	//构造函数传入序列化的class
-	public RpcEncoder(Class<?> genericClass) {
-		this.genericClass = genericClass;
+	public RpcEncoder(Class<?> cls) {
+		this.cls = cls;
 	}
 
 	@Override
-	protected void encode(ChannelHandlerContext channelHandlerContext, Object o, ByteBuf out) throws Exception {
+	protected void encode(ChannelHandlerContext channelHandlerContext, Object object, ByteBuf out) throws Exception {
 		//对象o序列化成二进制
-		if (genericClass.isInstance(o)){
-			byte[] data = SerializationUtil.serialize(o);
+		if (cls.isInstance(object)){
+			byte[] data = SerializationUtil.serialize(object);
 			out.writeInt(data.length);
 			out.writeBytes(data);
+		}else{
+			throw new Exception("对象不合法");
 		}
 	}
 }
